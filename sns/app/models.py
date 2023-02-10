42adb993249f0a4065f53a9c52e5edbbbeb51855
nyanchi <rotdicm@gmail.com>
2023-02-10 15:59:31 +0900

    形態素解析の処理や辞書などを追加した

--- sns/app/models.py
+++ sns/app/models.py
@@ -8,6 +8,7 @@ class MyText(models.Model):
 
     user = models.ForeignKey(AuthUser,on_delete=models.CASCADE)
     text = models.TextField(max_length=255,null=False,blank=False)
+    textpoint = models.TextField(max_length=255)
     created_datetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
 
--- sns/app/views.py
+++ sns/app/views.py
@@ -15,6 +15,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 import pprint
 
+import re
+import MeCab
+import pandas as pd
+import time
+
     
 class BaseView(LoginRequiredMixin,ListView):
     model = MyText
@@ -247,3 +252,64 @@ def UnFollowView(request,*args, **kwargs):
 class BaseFollowView(LoginRequiredMixin,ListView):
     model = RelateUser
     template_name = 'profile.html'
+
+#===================MeCab======================
+# analytext = MyText.objects.all()
+
+# pn_df = pd.read_csv('app/Dic/dic.txt',\
+#                     sep=':',
+#                     encoding='utf-8',
+#                     names=('Word','Reading','POS', 'PN')
+#                    )
+
+# m = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
+
+# def get_diclist(text):
+#     parsed = m.parse(text)
+#     lines = parsed.split('\n')
+#     lines = lines[0:-2]
+#     diclist = []
+#     for word in lines:
+#         l = re.split('\t|,',word)
+#         d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
+#         diclist.append(d)
+#     return(diclist)
+
+# word_list = list(pn_df['Word'])
+# pn_list = list(pn_df['PN'])
+# pn_dict = dict(zip(word_list, pn_list))
+
+# def add_pnvalue(diclist_old):
+#     diclist_new = []
+#     for word in diclist_old:
+#         base = word['BaseForm']
+#         if base in pn_dict:
+#             pn = float(pn_dict[base])
+#         else:
+#             pn = 'notfound'
+#         word['PN'] = pn
+#         diclist_new.append(word)
+#     return(diclist_new)
+
+# def get_pnmean(diclist):
+#     pn_list = []
+#     for word in diclist:
+#         pn = word['PN']
+#         if pn != 'notfound':
+#             pn_list.append(pn)           
+#     if len(pn_list) > 0:        
+#         pnmean = sum(pn_list)
+#     else:
+#         pnmean = 0
+#     return(pnmean)
+
+# start_time = time.time()
+# pnmeans_list = []
+# for tw in analytext['text']:
+#     dl_old = get_diclist(tw)
+#     dl_new = add_pnvalue(dl_old)
+#     pnmean = get_pnmean(dl_new)
+#     pnmeans_list.append(pnmean)
+# print(time.time() - start_time)
+
+
