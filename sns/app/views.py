bc6efb0d07668b1853341eb8479ee6a5e8b7f414
nyanchi <rotdicm@gmail.com>
2023-02-21 11:22:37 +0900

    mecabの処理をコメントアウト

--- sns/app/views.py
+++ sns/app/views.py
@@ -13,6 +13,7 @@ from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
+import Analysis
 import pprint
 
 import re
@@ -110,6 +111,8 @@ class TextCreateView(CreateView):
         object.save()
         return super().form_valid(form)
     
+    Analysis()
+    
     #=====================MeCab=====================
 
     # analytext = MyText.objects.get(text=MyText.text)
@@ -120,10 +123,11 @@ class TextCreateView(CreateView):
     #                     names=('Word','Reading','POS', 'PN')
     #                 )
 
-    # m = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
+    # mecab = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
     
-    # def get_diclist(text):
-    #     parsed = m.parse(text)
+    # def get_diclist(analytext):
+    #     mecab = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
+    #     parsed = mecab.parse(analytext)
     #     lines = parsed.split('\n')
     #     lines = lines[0:-2]
     #     diclist = []
@@ -313,64 +317,3 @@ def UnFollowView(request,*args, **kwargs):
 class BaseFollowView(LoginRequiredMixin,ListView):
     model = RelateUser
     template_name = 'profile.html'
-
-#===================MeCab======================
-# analytext = MyText.objects.get()
-
-# pn_df = pd.read_csv('app/Dic/dic.txt',\
-#                     sep=':',
-#                     encoding='utf-8',
-#                     names=('Word','Reading','POS', 'PN')
-#                    )
-
-# m = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
-
-# def get_diclist(text):
-#     parsed = m.parse(text)
-#     lines = parsed.split('\n')
-#     lines = lines[0:-2]
-#     diclist = []
-#     for word in lines:
-#         l = re.split('\t|,',word)
-#         d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
-#         diclist.append(d)
-#     return(diclist)
-
-# word_list = list(pn_df['Word'])
-# pn_list = list(pn_df['PN'])
-# pn_dict = dict(zip(word_list, pn_list))
-
-# def add_pnvalue(diclist_old):
-#     diclist_new = []
-#     for word in diclist_old:
-#         base = word['BaseForm']
-#         if base in pn_dict:
-#             pn = float(pn_dict[base])
-#         else:
-#             pn = 'notfound'
-#         word['PN'] = pn
-#         diclist_new.append(word)
-#     return(diclist_new)
-
-# def get_pnmean(diclist):
-#     pn_list = []
-#     for word in diclist:
-#         pn = word['PN']
-#         if pn != 'notfound':
-#             pn_list.append(pn)           
-#     if len(pn_list) > 0:        
-#         pnmean = sum(pn_list)
-#     else:
-#         pnmean = 0
-#     return(pnmean)
-
-# start_time = time.time()
-# pnmeans_list = []
-# for tw in analytext:
-#     dl_old = get_diclist(tw)
-#     dl_new = add_pnvalue(dl_old)
-#     pnmean = get_pnmean(dl_new)
-#     pnmeans_list.append(pnmean)
-# print(time.time() - start_time)
-
-# MyText.objects.create(textpoint=pnmean)
