52357958e404b47ed51960737ea216d7453b1f39
nyanchi <rotdicm@gmail.com>
2023-03-06 15:56:59 +0900

    json化未実装

--- sns/app/views.py
+++ sns/app/views.py
@@ -8,18 +8,15 @@ from django.views import generic
 from django.views.generic import CreateView,ListView,DetailView,TemplateView,UpdateView
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
 from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,JsonResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse
-from app import Analysis
+from app.Analysis import Analysis
 import pprint
-
-import re
-import MeCab
-import pandas as pd
-import time
+import json
+import numpy as np
 
     
 class BaseView(LoginRequiredMixin,ListView):
@@ -107,75 +104,25 @@ class TextCreateView(CreateView):
 
     def form_valid(self, form):
         object = form.save(commit=False)
+
         object.user = self.request.user
         object.save()
+        post_query_set = object
+        tex = np.array(post_query_set)
+        if tex.ndim == 0:
+            tex = [str(tex)]
+        print(tex)
+        post_json = JsonResponse(json.loads(str(tex)))
+        # post_json = serializers.serialize("json", tex)
+        # print(json.dump(post_json))
         return super().form_valid(form)
+
+    # print(person)
     
-    # Analysis()
+    #===========analysis===========
+    # morphologicalanalysis = Analysis()
     
-    #=====================MeCab=====================
 
-    # analytext = MyText.objects.get(text=MyText.text)
-
-    # pn_df = pd.read_csv('app/Dic/dic.txt',\
-    #                     sep=':',
-    #                     encoding='utf-8',
-    #                     names=('Word','Reading','POS', 'PN')
-    #                 )
-
-    # mecab = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
-    
-    # def get_diclist(analytext):
-    #     mecab = MeCab.Tagger('-d /opt/homebrew/lib/mecab/dic/ipadic')
-    #     parsed = mecab.parse(analytext)
-    #     lines = parsed.split('\n')
-    #     lines = lines[0:-2]
-    #     diclist = []
-    #     for word in lines:
-    #         l = re.split('\t|,',word)
-    #         d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
-    #         diclist.append(d)
-    #     return(diclist)
-
-    # word_list = list(pn_df['Word'])
-    # pn_list = list(pn_df['PN'])
-    # pn_dict = dict(zip(word_list, pn_list))
-
-    # def add_pnvalue(diclist_old):
-    #     diclist_new = []
-    #     for word in diclist_old:
-    #         base = word['BaseForm']
-    #         if base in pn_dict:
-    #             pn = float(pn_dict[base])
-    #         else:
-    #             pn = 'notfound'
-    #         word['PN'] = pn
-    #         diclist_new.append(word)
-    #     return(diclist_new)
-
-    # def get_pnmean(diclist):
-    #     pn_list = []
-    #     for word in diclist:
-    #         pn = word['PN']
-    #         if pn != 'notfound':
-    #             pn_list.append(pn)           
-    #     if len(pn_list) > 0:        
-    #         pnmean = sum(pn_list)
-    #     else:
-    #         pnmean = 0
-    #     return(pnmean)
-
-    # start_time = time.time()
-    # pnmeans_list = []
-    # for tw in analytext:
-    #     dl_old = get_diclist(tw)
-    #     dl_new = add_pnvalue(dl_old)
-    #     pnmean = get_pnmean(dl_new)
-    #     pnmeans_list.append(pnmean)
-    # print(time.time() - start_time)
-
-    # MyText.objects.create(textpoint=pnmean)
-    
     success_url = reverse_lazy('sns:base')
 
 def TextDeleteView(request,pk):
