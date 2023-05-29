48c8a3e8925d3dc5e69ba5a9057d453e0aab4cf6
nyanchi <rotdicm@gmail.com>
2023-05-29 15:54:16 +0900

    urlsのpathを変更

--- sns/sns/urls.py
+++ sns/sns/urls.py
@@ -23,7 +23,7 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('app.urls')),
+    path('apiiii/v1/', include('app.urls')),
     # re_path(r"^.*$", FrontBaseView.as_view()),
     path('accounts/',include('accounts.urls')),
     path('users/<uuid:activate_token>/activation/', activate_user, name='users-activation'),
