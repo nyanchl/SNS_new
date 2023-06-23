d51c15af7016a10f45c6e3c1d6e0aef97a56abf9
nyanchi <rotdicm@gmail.com>
2023-06-23 10:29:05 +0900

    Vue側からemailとpasswordを入力してaccesstokenとrefreshtokenを取得することができるよすることができるようになった

--- sns/frontend/src/components/Login.vue
+++ sns/frontend/src/components/Login.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="login">
-    <input type="text" v-model="email">
-    <input type="text" v-model="password">
-    <button @click="login">login</button><hr>
+    <form @submit.prevent="login">
+      <input type="text" v-model="email">
+      <input type="text" v-model="password">
+      <button type="submit">login</button>
+    </form>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   name: "LoginView",
@@ -20,20 +22,19 @@ export default {
         "refresh":'',
         "access":'',
       },
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     };
   },
   methods: {
-    login: function(){
+    login(){
       // token取得のためのusernameとpasswordセット
       const data = {email: this.email, password: this.password}
       // access_token&refresh_tokenを取得
-      this.axios
-        .post("http://localhost:8000/api-auth/jwt/",data)
+      axios
+        .post("http://localhost:8000/api-auth/jwt",data)
         // レスポンスを一旦tokensプロパティに格納
         .then(response => (this.tokens = response.data));
-        console.log(this.tokens);
     }
   },
 };
